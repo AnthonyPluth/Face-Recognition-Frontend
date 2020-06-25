15d757f27cf00d703e65c8cf1f5818a96016f858
Anthony Pluth <anthony.pluth@target.com>
2020-06-24 22:44:30 -0500

    update babelrc

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = (api) => {
+  if (api.cache.using(() => process.env.CYPRESS_ENV)) {
+    return {
+      plugins: [
+        [
+          "istanbul",
+          {
+            include: ["src/**/*.{js,jsx}"],
+            exclude: ["**/*.spec.{js,jsx}"],
+          },
+        ],
+      ],
+    };
+  }
+  return {
+    presets: ["react-app"],
+    plugins: ["transform-class-properties"],
+  };
+};
