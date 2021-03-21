4145c200fec4a627790af674d9354193cda62e73
Anthony Pluth <2065118-abpluth@users.noreply.gitlab.com>
2021-03-21 15:12:23 +0000

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,21 @@
-module.exports = (api) => {
-  if (api.cache.using(() => process.env.CYPRESS_ENV)) {
-    return {
-      plugins: [
-        [
-          "istanbul",
-          {
-            include: ["src/**/*.{js,jsx}"],
-            exclude: ["**/*.spec.{js,jsx}"],
-          },
-        ],
-      ],
-    };
-  }
-  return {
-    presets: ["react-app"],
-    plugins: ["transform-class-properties"],
-  };
+
+const instrumentForCodeCoverage = process.env.CYPRESS_ENV;
+
+/**
+ * Used by e2e tests. Instruments the code to expose coverage
+ * info to the browser
+ */
+const istanbulPlugin = [
+  'istanbul',
+  {
+    include: ["src/**/*.{js,jsx}"],
+    exclude: ["**/*.spec.{js,jsx}"],
+  },
+];
+
+module.exports = {
+  presets: ["react-app"],
+  plugins: ["transform-class-properties",
+    ...(instrumentForCodeCoverage ? [istanbulPlugin] : []),
+  ],
 };
